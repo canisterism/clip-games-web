fa0c943c247f837c09970dd6548d4d8e4a74a815
canisterism <ohtthstc@gmail.com>
2022-04-02 17:20:58 +0900

    yarn tailwindcss init -p

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
