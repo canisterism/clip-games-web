b3f4887415a70926b9ae91c087581a5f88358ba0
canisterism <ohtthstc@gmail.com>
2022-04-02 19:58:03 +0900

    add domains for image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["picsum.photos"],
+    domains: [
+      "picsum.photos",
+      "storage.googleapis.com",
+      "https://placehold.jp",
+    ],
   },
   reactStrictMode: true,
 };
