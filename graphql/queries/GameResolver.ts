5b55250e0184dfebfb982f1ff381f360e8d4a78a
canisterism <ohtthstc@gmail.com>
2022-10-11 20:13:39 +0900

    wip

--- graphql/queries/GameResolver.ts
+++ graphql/queries/GameResolver.ts
@@ -1,6 +1,9 @@
+import { findManyCursor } from "@/graphql/helpers/prisma/findManyCursor";
 import { Game } from "@/graphql/resources/Game";
+import { GameConnection } from "@/graphql/resources/GameConnection";
 import { PrismaClient } from "@prisma/client";
 import { Arg, Ctx, Query, Resolver } from "type-graphql";
+import { ConnectionArgs } from "./../resources/Connection/types";
 
 @Resolver((of) => Game)
 export class GameResolver {
@@ -24,5 +27,15 @@ export class GameResolver {
       },
     });
     return game;
+  }
+
+  @Query((returns) => GameConnection, { nullable: true })
+  async gamesConnection(
+    @Arg("args") connectionArgs: ConnectionArgs,
+    @Ctx("prisma") prisma: PrismaClient
+  ) {
+    findManyCursor(args => prisma.game.findMany({...args}))
+
+    }
   }
 }
