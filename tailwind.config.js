6d13b1bb2d84b76520f664ebbac781712543e16a
canisterism <ohtthstc@gmail.com>
2022-10-14 23:00:58 +0900

    yarn add tailwindcss && npx tailwindcss init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  plugins: [],
+};
