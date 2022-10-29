d03a2939e72e73373b4c64cd5540e82ab3a49985
canisterism <ohtthstc@gmail.com>
2022-10-29 20:50:30 +0900

    specify graphql schema file
    update next to use standalone mode to run application on cloudrun

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     domains: ["picsum.photos", "storage.googleapis.com", "placeimg.com"],
   },
   reactStrictMode: true,
+  output: "standalone",
 };
 
 module.exports = nextConfig;
