22eda53a3992042015d1bafb45c76822a790dd7e
canisterism <ohtthstc@gmail.com>
2022-11-03 18:37:22 +0900

    now genre seed script can sanitize dirty string

--- src/backend/prisma/seeds/genre.ts
+++ src/backend/prisma/seeds/genre.ts
@@ -6,8 +6,11 @@ export const importGenres = async (prisma: PrismaClient) => {
   const games = await fetchDocumentDataList("games");
 
   for (const [_, game] of Object.entries(games)) {
-    const genres = game.genre.split("/");
-    for (const genre of genres) {
+    if (!game.genre) {
+      continue;
+    }
+    const genres = splitGenre(game.genre as string);
+    for (let genre of genres) {
       await prisma.genre.upsert({
         where: {
           id: genre,
@@ -22,26 +25,67 @@ export const importGenres = async (prisma: PrismaClient) => {
   }
 };
 
-const GENRES_MAP = {
-  ADV: "アドベンチャー",
+export const splitGenre = (rawString: string): string[] =>
+  rawString
+    .split(/(?:\+)|(?:\&)|(?:\/)/) // ? & / は区切り文字なのでsplitする
+    .flat()
+    .map((str) => str.replace(/(\.)|(他)|(3D)|\s/, "")) // . 他 3D 空白などは扶養なので削除
+    .filter((v) => v?.length > 0)
+    .map((str) =>
+      Object.keys(INVALId_GENRE_MAP).includes(str)
+        ? INVALId_GENRE_MAP[str as keyof typeof INVALId_GENRE_MAP]
+        : str
+    )
+    .filter((v) => v) as string[];
+
+// マスタ側のデータが間違ってるなどの理由で修正されるべきジャンルのキー名と正しいキー名のマッピング
+export const INVALId_GENRE_MAP = {
+  SPRG: "SRPG",
+  PZLl: "PZL",
+  SPT: "SPG",
+  ボードゲーム: "BG",
+  AVG: "ADV",
+  ATC: "ACT",
+  ETC: "etc",
+  RST: undefined,
+  ACR: undefined,
+  act: "ACT",
+  RCE: "RCG",
+  SRG: "SLG",
+  SLC: "SLG",
+  BDG: "BG",
+};
+
+export const GENRES_MAP = {
   ACT: "アクション",
-  AADV: "アクションアドベンチャー",
+  PZL: "パズル",
+  TBL: "テーブルゲーム",
   RPG: "RPG",
+  クイズ: "クイズ",
   ARPG: "アクションRPG",
-  音楽: "音楽",
-  SLG: "シミュレーション",
+  ADV: "アドベンチャー",
+  AADV: "アクションアドベンチャー",
+  STG: "シューティング",
   RCG: "レース",
-  FTG: "格闘ゲーム",
+  SLG: "シミュレーション",
+  etc: "その他",
   SRPG: "シミュレーションRPG",
   SPG: "スポーツ",
-  TBL: "テーブルゲーム",
-  SPRG: "シミュレーションRPG(ToBeFixed)",
-  SLC: "恋愛シミュレーション",
-  STG: "シューティング",
-  PZL: "パズル",
-  TPS: "TPS",
+  FTG: "格闘ゲーム",
+  APZL: "アクションパズル",
+  音楽: "音楽",
+  RTS: "リアルタイムストラテジー",
   FPS: "FPS",
+  TPS: "TPS",
+  BG: "ボードゲーム",
+  ダンスシミュレーション: "ダンスシミュレーション",
+  FACT: "格闘アクション",
+  キャラ: "キャラゲー",
   ASTG: "アクションシューティング",
-  etc: "その他",
+  脱衣麻雀: "脱衣麻雀",
+  RPG制作: "RPG制作",
   RACT: "レースアクション",
+  FPA: "ファーストパーソンアドベンチャー",
+  TCG: "トレーディングカードゲーム",
+  サバイバル: "サバイバル",
 };
