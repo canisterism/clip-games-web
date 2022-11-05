780f1a99111194ad313b75df287eb091a7fe1d5e
canisterism <ohtthstc@gmail.com>
2022-11-05 14:43:53 +0900

    ignore invalid import cases

--- src/backend/prisma/seeds/clip.ts
+++ src/backend/prisma/seeds/clip.ts
@@ -1,22 +1,36 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { fetchSubCollection } from "./utils";
 
 export const importClips = async (prisma: PrismaClient) => {
   const clips = await fetchSubCollection("clips");
   for (const clip of clips) {
-    await prisma.clip.upsert({
-      where: {
-        gameId_profileId: {
+    console.log(`clip.game.ref.id: ${clip.game.ref.id}`);
+    console.log(`clip.profile.ref.id: ${clip.profile.ref.id}`);
+    try {
+      await prisma.clip.upsert({
+        where: {
+          gameId_profileId: {
+            gameId: clip.game.ref.id,
+            profileId: clip.profile.ref.id,
+          },
+        },
+        create: {
           gameId: clip.game.ref.id,
           profileId: clip.profile.ref.id,
+          createdAt: clip.createdAt.toDate(),
         },
-      },
-      create: {
-        gameId: clip.game.ref.id,
-        profileId: clip.profile.ref.id,
-        createdAt: clip.createdAt.toDate(),
-      },
-      update: {},
-    });
+        update: {},
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === "P2003" // 対象のゲームかユーザーが存在しないケース
+      ) {
+        console.error("Failed to import this clip: ");
+        console.error({ clip });
+        continue;
+      }
+      throw error;
+    }
   }
 };
--- src/backend/prisma/seeds/reviewLike.ts
+++ src/backend/prisma/seeds/reviewLike.ts
@@ -1,5 +1,5 @@
 import { fetchSubCollection } from "@/src/backend/prisma/seeds/utils";
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 export const importReviewLikes = async (prisma: PrismaClient) => {
   const likes = await fetchSubCollection("likes");
@@ -7,34 +7,49 @@ export const importReviewLikes = async (prisma: PrismaClient) => {
     if (!like.author || !like.viewer || !like.game) {
       continue;
     }
-    const review = await prisma.review.findFirstOrThrow({
-      where: {
-        gameId: like.game.ref.id,
-        profileId: like.author.ref.id,
-      },
-    });
+    try {
+      const review = await prisma.review.findFirst({
+        where: {
+          gameId: like.game.ref.id,
+          profileId: like.author.ref.id,
+        },
+      });
+      if (!review) {
+        continue;
+      }
 
-    await prisma.reviewLike.upsert({
-      where: {
-        reviewId_profileId: {
-          reviewId: review.id,
-          profileId: like.viewer.ref.id,
-        },
-      },
-      create: {
-        review: {
-          connect: {
-            id: review.id,
+      await prisma.reviewLike.upsert({
+        where: {
+          reviewId_profileId: {
+            reviewId: review.id,
+            profileId: like.viewer.ref.id,
           },
         },
-        profile: {
-          connect: {
-            id: like.viewer.ref.id,
+        create: {
+          review: {
+            connect: {
+              id: review.id,
+            },
           },
+          profile: {
+            connect: {
+              id: like.viewer.ref.id,
+            },
+          },
+          createdAt: like.createdAt.toDate(),
         },
-        createdAt: like.createdAt.toDate(),
-      },
-      update: {},
-    });
+        update: {},
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === "P2025" // 対象のレビューかプロフィールが存在しないケース
+      ) {
+        console.error("Failed to import this like: ");
+        console.error({ like });
+        continue;
+      }
+      throw error;
+    }
   }
 };
