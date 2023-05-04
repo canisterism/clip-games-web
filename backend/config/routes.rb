cbe50ade796a3b4d9f435b4f9333da7d46dffa5e
canisterism <ohtthstc@gmail.com>
2023-05-04 22:43:39 +0900

    graphiql使うためにゴニョった
    https://github.com/rmosolgo/graphiql-rails

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
