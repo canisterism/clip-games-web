c5d06508157c93b778d192599847d2ea47495bb4
canisterism <ohtthstc@gmail.com>
2023-07-22 20:36:54 +0900

    vercelは許可する

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001", "https://clip-games.com", "https://clip-games-web.vercel.app"
+    origins "http://localhost:3001", "https://clip-games.com", "https://clip-games*.vercel.app"
 
     resource "*",
       headers: :any,
