c011e2bc886ad2120936f5ab84034a693bda66ff
canisterism <ohtthstc@gmail.com>
2023-07-22 20:34:48 +0900

    検索バーのウィジェットをtailwind-uiのやつに置き換えた

--- frontend/components/layouts/SideBarLayout.tsx
+++ frontend/components/layouts/SideBarLayout.tsx
@@ -1,4 +1,5 @@
 import ProfileMenuIcon from "@/components/ProfileMenuIcon";
+import SearchBox from "@/components/SearchBox";
 import { Dialog, Transition } from "@headlessui/react";
 import { FireIcon, MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import {
@@ -10,7 +11,6 @@ import {
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import React, { Fragment, useState } from "react";
-import { SearchBox } from "react-instantsearch-hooks-web";
 
 const navigation = [
   { name: "ホーム", href: "#", icon: HomeIcon, current: true },
@@ -102,30 +102,6 @@ function LogoLink() {
         clip-games
       </a>
     </Link>
-  );
-}
-
-function SearchBar(props: { onSearch: (query: string) => void }) {
-  return (
-    <form className="relative flex flex-1" action="#" method="GET">
-      <label htmlFor="search-field" className="sr-only">
-        Search
-      </label>
-      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-        <MagnifyingGlassIcon
-          className="h-5 w-5 text-gray-400"
-          aria-hidden="true"
-        />
-      </div>
-      <input
-        id="search-field"
-        className="block h-full w-full border-0 rounded-md py-1.5 pl-10 pr-3 bg-gray-700 text-gray-200 placeholder:text-gray-400 focus:bg-white focus:text-gray-900 focus:ring-0 sm:text-sm"
-        placeholder="Search..."
-        type="search"
-        name="search"
-        onChange={(e) => props.onSearch(e.target.value)}
-      />
-    </form>
   );
 }
 
